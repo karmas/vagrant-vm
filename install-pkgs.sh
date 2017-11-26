acdf508885917c44480008a301c15a062014303f
karma <karma>
2017-11-26 16:55:02 -0500

    added jdk

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -2,4 +2,5 @@ yum -y install kernel-devel # needed for virtualbox guest additions
 yum -y groupinstall 'development tools'
 yum -y install epel-release # needed for many packages e.g. tmux xfce
 yum -y install tmux vim
+yum -y install java-1.8.0-openjdk-devel
 yum -y groupinstall X11
