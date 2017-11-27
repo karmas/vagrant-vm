4380ead05a895065c37cbafd271643e50f9deee1
k.shrestha <kshrestha@velatt.com>
2017-11-27 12:56:24 -0500

    fix installer

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,8 +1,7 @@
 yum -y install kernel-devel # needed for virtualbox guest additions
 yum -y groupinstall 'development tools'
 yum -y install epel-release # needed for many packages e.g. tmux xfce
-yum -y install tmux vim
+yum -y install tmux vim man-pages wget htop
+yum -y install sysstat # pidstat and more
 yum -y install java-1.8.0-openjdk-devel
 yum -y groupinstall X11
-yum -y man-pages wget htop
-yum -y sysstat # pidstat and more
